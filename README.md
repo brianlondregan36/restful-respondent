ab78b62c9574f6316ae376a21b3db21cd5a413e8
brianlondregan36 <brian.londregan@gmail.com>
2019-09-19 11:03:18 -0400

    cleaning up the directory again

---
+++ README.md
@@ -0,0 +1,16 @@
+# Python REST API demo
+
+[Try it out here](https://restful-respondent.herokuapp.com/)
+
+"views.py" under the "respondents" folder has the API calls. 
+The page markup is under "templates" folder and javascript is under "static" folder. 
+I deployed and hosted the application using Heroku. 
+
+### 6/13/19 --- 
+
+So far the demo only uses the Surveys API...
+
+1. authentication 
+2. on page load flash survey's description onto the page. Using GET/{surveyId}
+3. user fills out a form to create a respondent. Adding the record via POST /{surveyId}/respondents
+4. flash that new respondent's survey link onto the page. Using GET /{surveyId}/respondents/{respId} 
---
+++ respondents/views.py
@@ -0,0 +1,67 @@
+from respondents import app
+from respondents.passwords import clientid, clientsecret
+from flask import Flask, render_template, request
+from flask_cors import CORS
+import base64, json, requests
+
+
+@app.route('/', methods=['GET'])
+def index():
+    access_token = ConfirmitAuthenticate()
+    req = requests.get('https://ws.testlab.firmglobal.net/v1/surveys/p10210620', headers = {"Content-Type": "application/x-www-form-urlencoded", "authorization": access_token})
+    if req.status_code == 200:
+        respText = json.loads(req.text)
+        desc = respText["description"]
+    result = ""
+    if desc is not None:
+        result = "Survey Description: " + desc
+    return render_template('index.html', result=result)
+
+@app.route('/Response', methods=['POST'])
+def CreateSurveyResponse():
+    options = request.form['options']
+    email = request.form['email']
+    if options is not None and email is not None:
+        respValues = {"email": email, "options": options}
+        access_token = ConfirmitAuthenticate()
+        req = requests.post('https://ws.testlab.firmglobal.net/v1/surveys/p10210620/respondents',
+                         data = json.dumps(respValues),
+                         headers = {"Content-Type": "application/json", "Accept": "application/json", "authorization": access_token}
+                         )
+        if req.status_code == 201:
+            respText = json.loads(req.text)
+            links = respText["links"]
+            nextUrl = links["self"]
+            surveyLink = GetRespondentLink(nextUrl, access_token)
+            result = {"surveyLink": surveyLink}
+            return json.dumps(result)
+        else:
+            return requests.exceptions.RequestException
+    else:
+        #client-side error
+        return None
+
+
+
+def ConfirmitAuthenticate():
+    endpoint = "https://author.testlab.firmglobal.net/identity/connect/token"
+    grant_scope = {"grant_type": "api-user", "scope": "pub.surveys pub.hubs"}
+    req = requests.post(endpoint, data=grant_scope, auth=(clientid, clientsecret))
+    if req.status_code == 200:
+        respText = json.loads(req.text)
+        access_token = respText["token_type"] + " " + respText["access_token"]
+        return access_token
+    else:
+        return None
+
+def GetRespondentLink(url, token):
+    url = url + "?includeSurveyLink=true"
+    req = requests.get(url, headers = {"Content-Type": "application/x-www-form-urlencoded", "authorization": token})
+    if req.status_code == 200:
+        respText = json.loads(req.text)
+        links = respText["links"]
+        surveyLink = links["surveyLink"]
+        return surveyLink
+    else:
+        return None
+
